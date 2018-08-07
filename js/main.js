62ca82dfc8c89d14a931fee43ba6b1ece70eb6b8
WillFlapsGaming <42186857+WillFlapsGaming@users.noreply.github.com>
2018-08-07 16:36:19 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ function main() {
   $(this).toggleClass('active');
    window.location.href="./about.html"
   });
+  $('.trailer').on('click', function() {
+ 
+   window.location.href="https://www.youtube.com/watch?v=BvcICF7BqzU&feature=youtu.be"
+  });
   $('.back-to-home').on('click', function() {
   $(this).toggleClass('active');
 window.location.href="./index.html"
