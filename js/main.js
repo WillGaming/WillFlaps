452ef312ef945624e0fb5441f14db52b506b87a9
WillFlapsGaming <42186857+WillFlapsGaming@users.noreply.github.com>
2018-08-07 16:11:22 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@ function main() {
   $('.about').on('click', function() {
   $(this).toggleClass('active');
    window.location.href="./about.html"
- 
+  });
+  $('.back-to-home').on('click', function() {
+  $(this).toggleClass('active');
+window.location.href="./index.html"
    	  
 	});
   $('.projects-button').on('click', function() {
